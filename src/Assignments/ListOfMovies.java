7e9b6d6b03eb8aa07fcf487ee4f852cad80a25e4
Harini Reddy <harinireddy75@gmail.com>
2025-05-28 21:22:05 +0530

    collections

--- src/Assignments/ListOfMovies.java
+++ src/Assignments/ListOfMovies.java
@@ -8,7 +8,7 @@ public class ListOfMovies {
 public static void main(String[] args) {
 List<String> Movies = new LinkedList<String>();
 Movies.add("Dangal");
-Movies.add("Baahubali");
+Movies.add("Baahubali2");
 Movies.add("Pushpa");
 Movies.add("KGF");
 Movies.add("RRR");
